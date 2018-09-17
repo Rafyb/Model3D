964fa2977c6343134a9932ceffd26471cdda5c93
Danglotc <clement2311@hotmail.fr>
2018-09-17 10:46:44 +0200

    Point - Segment - Face ( en cours )

---
+++ src/Structure/Face.java
@@ -0,0 +1,7 @@
+package Structure;
+
+public class Face {
+	Point a, b, c;
+	
+
+}
---
+++ src/Structure/Segment.java
@@ -0,0 +1,29 @@
+package Structure;
+
+public class Segment {
+		Point x, y;
+		
+		public Segment(Point c, Point d) {
+			x = c;
+			y = d;
+		}
+
+		public Point getX() {
+			return x;
+		}
+
+		public void setX(Point x) {
+			this.x = x;
+		}
+
+		public Point getY() {
+			return y;
+		}
+
+		public void setY(Point y) {
+			this.y = y;
+		}
+		
+		
+
+}

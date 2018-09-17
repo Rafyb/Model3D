1b0ae2344a2b3e3a7e823c7417636697fce74afd
BauvinRaphael <raphael.bauvin@etudiant.univ-lille1.fr>
2018-09-17 11:09:30 +0200

    Fenetre IHM

--- src/Controlleur/GestionAffichage.java
+++ src/Controlleur/GestionAffichage.java
@@ -1,5 +1,44 @@
 package Controlleur;
 
-public class GestionAffichage {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
+public class GestionAffichage extends Application {
+
+	  public void start(Stage stage) {
+	    GridPane root = new GridPane();
+	    
+	    Button droite = new Button(">");
+	    Button gauche = new Button("<");
+	    Button haut = new Button("^");
+	    Button bas = new Button("v");
+	    
+	    droite.setMinSize(50, 50);
+	    gauche.setMinSize(50, 50);
+	    haut.setMinSize(50, 50);
+	    bas.setMinSize(50, 50);
+	    
+
+	    root.add(gauche, 0, 1);
+	    root.add(bas, 1, 1);
+	    root.add(droite, 2, 1);
+	    root.add(haut, 1, 0);
+	    
+
+	    Scene scene = new Scene(root, 150, 100);
+	    stage.setScene(scene);
+	    //stage.setFullScreen(true);
+	    stage.setTitle("Model 3D Move");
+	    stage.setResizable(false);
+	    stage.show();
+
+	  }
+
+	  public static void main(String[] args) {
+	    Application.launch(args);
+	  }
 }
+
--- src/Vue/Affichage.java
+++ src/Vue/Affichage.java
@@ -1,5 +1,34 @@
 package Vue;
 
-public class Affichage {
+import Controlleur.GestionAffichage;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Affichage extends Application {
+
+	  public void start(Stage stage) {
+	    VBox root = new VBox();
+	    
+	    GestionAffichage direction = new GestionAffichage();
+	    
+
+	    Scene scene = new Scene(root, 500, 500);
+	    stage.setScene(scene);
+	    //stage.setFullScreen(true);
+	    stage.setTitle("Model 3D Afficheur");
+	    stage.setResizable(false);
+	    stage.show();
+	    direction.start(new Stage());
+
+	  }
+
+	  public static void main(String[] args) {
+	    Application.launch(args);
+	  }
 
 }
+

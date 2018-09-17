4b7a61c2e331ce886e4a975273c7359c5eeb575b
BauvinRaphael <raphael.bauvin@etudiant.univ-lille1.fr>
2018-09-17 11:46:40 +0200

    on va faire du graphic context

--- src/Controlleur/GestionAffichage.java
+++ src/Controlleur/GestionAffichage.java
@@ -1,6 +1,8 @@
 package Controlleur;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
@@ -16,12 +18,22 @@ public class GestionAffichage extends Application {
 	    Button haut = new Button("^");
 	    Button bas = new Button("v");
 	    
+	    Button zoom = new Button("+");
+	    Button dezoom = new Button("-");
+	    
+	    zoom.setMinSize(35, 35);
+	    dezoom.setMinSize(35, 35);
+	    
+	    
 	    droite.setMinSize(50, 50);
 	    gauche.setMinSize(50, 50);
 	    haut.setMinSize(50, 50);
 	    bas.setMinSize(50, 50);
 	    
-
+	    
+	    root.add(zoom, 0, 0);
+	    root.add(dezoom, 2, 0);
+	    
 	    root.add(gauche, 0, 1);
 	    root.add(bas, 1, 1);
 	    root.add(droite, 2, 1);
@@ -30,7 +42,6 @@ public class GestionAffichage extends Application {
 
 	    Scene scene = new Scene(root, 150, 100);
 	    stage.setScene(scene);
-	    //stage.setFullScreen(true);
 	    stage.setTitle("Model 3D Move");
 	    stage.setResizable(false);
 	    stage.show();
--- src/Vue/Affichage.java
+++ src/Vue/Affichage.java
@@ -2,23 +2,39 @@ package Vue;
 
 import Controlleur.GestionAffichage;
 import javafx.application.Application;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Affichage extends Application {
 
-	  public void start(Stage stage) {
-	    VBox root = new VBox();
-	    
+	  public void start(Stage stage) { 
+		VBox root = new VBox();
 	    GestionAffichage direction = new GestionAffichage();
+	    Canvas canvas = new Canvas (300, 300);
+	    GraphicsContext gc = canvas.getGraphicsContext2D();
+	    root.getChildren().add(canvas);
+        root.setStyle("-fx-background-color: lightgrey;"); 
+        
+	    
 	    
 
-	    Scene scene = new Scene(root, 500, 500);
+	    Scene scene = new Scene(root);
+	    scene.setCamera(new PerspectiveCamera());
+	    
+	    
+
+	    
+	    
+	    
+	    
 	    stage.setScene(scene);
-	    //stage.setFullScreen(true);
 	    stage.setTitle("Model 3D Afficheur");
 	    stage.setResizable(false);
 	    stage.show();

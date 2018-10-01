a5a778231acf5e0c13276b1185092af51438ce1d
Lucas Breviere <lucasbreviere@yahoo.fr>
2018-10-01 10:40:42 +0200

    *

--- test/testUnitaire/TriTest.java
+++ test/testUnitaire/TriTest.java
@@ -4,6 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import com.sun.swing.internal.plaf.metal.resources.metal;
+
+import Modele.Modele;
+import Structure.ReadModele;
+import Tri.TriRapide;
+
 public class TriTest {
+	TriRapide t = new TriRapide();
+	Modele m ;
+	@Before
+	void initialiser() {
+		m = new Modele(new ReadModele("ressources/3carre.ply"));
+	}
 
 }

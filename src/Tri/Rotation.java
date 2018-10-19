9befdc790696affdfcb22c5278b0189e195b82c3
Danglotc <clement2311@hotmail.fr>
2018-10-19 14:35:33 +0200

    Rotation setup

--- src/Tri/Rotation.java
+++ src/Tri/Rotation.java
@@ -5,12 +5,12 @@ import Structure.Face;
 import Structure.Point;
 /**
  * Permet d'effectuer les calculs des operations de mouvement sur le modele : rotation, zoom et translation.
- * @author canonnet
+ * @author danglotc
  */
-public class Rotation {
-
-	public Rotation() {
-	}
+public class Rotation {	
+	/**
+	 * Permet de faire la rotation autour de l'axe X avec un nagle en radian.
+	 */
 
 	public void rotationX(Modele m, int radian) {
 		for(int i = 0 ; i < m.getAllPoints().length;i++) {
@@ -24,6 +24,9 @@ public class Rotation {
 			m.getPointAtIndex(i).setZ((y*sin)+(z*cos));
 		}
 	}
+	/**
+	 * Permet de faire la rotation autour de l'axe Y avec un angle en radian.
+	 */
 
 	public void rotationY(Modele m, int radian) {
 		for(int i = 0 ; i < m.getAllPoints().length;i++) {
@@ -37,6 +40,9 @@ public class Rotation {
 			m.getPointAtIndex(i).setZ((-sin)*x+z*cos);
 		}
 	}
+	/**
+	 * Permet de faire la rotation autour de l'axe Z avec un angle en radian.
+	 */
 	public void rotationZ(Modele m, int radian) {
 		for(int i = 0 ; i < m.getAllPoints().length;i++) {
 			double cos = Math.cos(Math.toRadians(radian));
@@ -49,7 +55,9 @@ public class Rotation {
 			m.getPointAtIndex(i).setY(sin*x+cos*y);
 		}
 	}
-	
+	/**
+	 * Zoomer la figure selon un paramètre de zoom donné.
+	 */
 	public void zoom(Modele m, double coef) {
 		for(int i = 0 ; i < m.getAllPoints().length;i++) {
 
@@ -62,6 +70,9 @@ public class Rotation {
 			m.getPointAtIndex(i).setZ(z*(1+coef));
 		}
 	}
+	/**
+	 * Dezoomer la figure selon un paramètre de dezoom donné. 
+	 */
 	public void dezoom(Modele m, double coef) {
 		for(int i = 0 ; i < m.getAllPoints().length;i++) {
 
@@ -74,7 +85,9 @@ public class Rotation {
 			m.getPointAtIndex(i).setZ(z*(1-coef));
 		}
 	}
-	
+	/**
+	 * Methode permettant de faire les transitions de la figure.
+	 */
 	public void translation(Modele m, Point p) {
 		for(int i = 0 ; i < m.getAllPoints().length;i++) {
 

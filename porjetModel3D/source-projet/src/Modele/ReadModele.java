2fe7f84ad15c15e5cfbd49d4a26fc130007fb53c
Raphael <Raphael@192.168.1.100>
2018-11-05 22:41:43 +0100

    Presque livrable 1

---
+++ porjetModel3D/source-projet/src/Modele/ReadModele.java
@@ -0,0 +1,150 @@
+package Modele;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Lit un fichier de format .ply et genere la liste de point et de face du modele.
+ * @author danglotc
+ */
+public class ReadModele {
+	private File f;
+	private BufferedReader br;
+
+	private int nbPoint;
+	private int nbFace;
+
+	private Point[] points;
+	private Face[] faces;
+
+
+	/**
+	 * Constructeur prenant en @param l'adresse d'un fichier, génère un objet ReadModele contenant les faces et les points lues sur le fichier.
+	 *  
+	 */
+	public ReadModele(String fichier) {
+		if(ReadModele.fileExist(fichier)) {
+			f = new File(fichier);
+
+			try {
+				br = new BufferedReader(new FileReader(f));
+			} catch (FileNotFoundException e) {
+				System.out.println(e);
+			}
+
+			nbPoint = this.ajoutNbPoints();
+			nbFace = this.ajoutNbFaces();
+			this.endHeader();
+			this.insertPoints(nbPoint);
+			this.insertFaces(nbFace);
+		}
+
+
+	}
+
+	public static boolean fileExist(String path) {
+		return new File(path).exists();
+	}
+	public int ajoutNbPoints() {
+		String st ="";
+		int res;
+		do {
+			try {
+				st = br.readLine();
+			} catch (IOException e) {
+				System.out.println("element vertex bug");
+			}
+		} while (!st.contains("element vertex"));
+		res = Integer.parseInt(st.substring(15));
+		return res;
+
+	}
+	public int ajoutNbFaces() {
+		String st = "";
+		int res;
+		do {
+			try {
+				st = br.readLine();
+			} catch (IOException e) {
+				System.out.println("element face bug");
+			}
+		} while (!st.contains("element face"));
+		res = Integer.parseInt(st.substring(13));
+		return res;
+	}
+	
+	public int getNbPoints() {
+		return nbPoint;
+	}
+
+	public int getNbFaces() {
+		
+		return nbFace;
+	}
+	
+	public String endHeader() {
+		String st = "";
+		do {
+			try {
+				st = br.readLine();
+			} catch (IOException e) {
+				System.out.println("end header bug");
+			}
+		} while (!st.contains("end_header"));
+		return st;
+	}
+
+	public Point[] insertPoints(int nbPoint) {
+		points = new Point[nbPoint];
+		int cpt = 0;
+		String st = "";  
+		while (cpt < nbPoint) {
+			try {
+				st = br.readLine();
+				String[] p = st.split(" ");
+				if (p.length == 3)
+					points[cpt] = new Point(Double.parseDouble(p[0]), Double.parseDouble(p[1]), Double.parseDouble(p[2]));
+				else 
+					points[cpt] = new Point(Double.parseDouble(p[0]), Double.parseDouble(p[1]), Double.parseDouble(p[2]),Double.parseDouble(p[3]), Double.parseDouble(p[4]), Double.parseDouble(p[5]));
+				cpt ++;
+			} catch (IOException e) {
+				System.out.println("point bug bug");
+			}
+		}
+		return points;
+	}
+
+	public Face[] insertFaces(int nbFace) {
+		faces = new Face[nbFace];
+		int cpt = 0;
+		String st = "";
+		while (cpt < nbFace) {
+			try {
+				st = br.readLine();
+				String[] p = st.split(" ");
+				faces[cpt] = new Face(points[Integer.parseInt(p[1])], points[Integer.parseInt(p[2])], points[Integer.parseInt(p[3])]);
+				cpt ++;
+			} catch (IOException e) {
+				System.out.println("point bug bug");
+			}
+		}
+		return faces;
+	}
+	/**
+	 * Récupérer les points.
+	 */
+	public Point[] getPoint(){	
+		return points;
+	}
+	/**
+	 * Récupérer les faces.
+	 */
+	public Face[] getFace(){	
+		return faces;
+	}	
+}
+
+
---
+++ porjetModel3D/source-projet/src/Vue/Affichage.java
@@ -0,0 +1,157 @@
+package Vue;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import Controlleur.GestionAffichage;
+import Modele.Face;
+import Modele.Modele;
+import Modele.Point;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+/**
+ * La vue du modele,fonctionne sous la forme de Observer/Observable.
+ * Fenetre affichant le modele en 3D à l'aide d'une methode de dessin de triangle.
+ * Gere aussi les rotations a la souris 
+ * > x et y
+ * > x seulement avec ctrl enfonce
+ * > y seulement avec shift enfonce
+ * @author bauvinr canonnet danglotc
+ */
+public class Affichage implements Observer {
+	private GraphicsContext gc;
+	private Canvas canvas;
+	private Modele mod;
+	private double x,y;
+	/**
+	 * Constructeur de la classe Affichage.
+	 *  @param modele = attribut mod.
+	 */
+	public Affichage(Modele modele) {
+		mod = modele;
+		mod.addObserver(this);
+		x=y=0;
+	}
+	/**
+	 * Methode principale pour l'affichage qui crée les fenêtres.
+	 * 
+	 */
+	public void start(Stage stage) { 
+	
+		//////////////////////////// SCENES ////////////////////////////
+		HBox root = new HBox();
+		GestionAffichage boutons = new GestionAffichage(mod);
+		boutons.start(stage);
+		canvas = new Canvas (1150, 800);
+		gc = canvas.getGraphicsContext2D();
+		
+		root.getChildren().addAll(canvas,boutons.getPage());
+		Scene scene = new Scene(root);
+		
+		
+		
+		/////////////////////// MOUVEMENTS SOURIS ///////////////////////
+		canvas.setOnMouseDragged(e->{
+			
+			if (e.isPrimaryButtonDown()) {
+			if(this.x-e.getX()<0 && !e.isShiftDown()) {
+				mod.rotationY(2);
+			} else if (this.x-e.getX()>0  && !e.isShiftDown()) {
+				mod.rotationY(-2);
+			}
+			
+			if(this.y-e.getY()<0 && !e.isControlDown()) {
+				
+				mod.rotationX(-2);
+			} else if (this.y-e.getY()>0 && !e.isControlDown()) {
+				
+				mod.rotationX(2);
+			}
+			this.x = e.getX();
+			this.y = e.getY();
+			} else if (e.isSecondaryButtonDown()) {
+				Point p = new Point(-(this.x-e.getX()),-(this.y-e.getY()),0 );
+				mod.translation(p);
+				System.out.println(this.x + "   "+ e.getX() + " " + this.y  + "  " + e.getY());
+				this.x = e.getX();
+				this.y = e.getY();
+			}
+		});
+		
+		canvas.setOnMousePressed(e ->{this.x = e.getX();this.y = e.getY();});
+		
+		canvas.setOnScroll(e -> {
+			if(e.getDeltaY() > 0) {
+				mod.zoom(0.1);
+			} else if (e.getDeltaY() < 0){
+				mod.dezoom(0.1);
+			}
+		});
+		
+		canvas.setOnKeyPressed(e -> {
+			System.out.println(e.getCode());
+			if (e.getCode().equals(KeyCode.LEFT)) {
+				mod.rotationY(-10);
+			} else if (e.getCode().equals(KeyCode.UP)) {
+				mod.rotationX(10);
+			} else if (e.getCode().equals(KeyCode.RIGHT)) {
+				mod.rotationY(10);
+			} else if (e.getCode().equals(KeyCode.DOWN)) {
+				mod.rotationX(10);
+			}
+		});
+
+		////////////////////////// EXECUTION ///////////////////////////
+		mod.triZ();
+		triangle();
+
+		stage.setScene(scene);
+		stage.setTitle("Model 3D Afficheur");
+		stage.setResizable(false);
+		stage.show();
+
+		
+			
+		
+	}
+
+	/**
+	 * Methode pour dessiner les triangles.
+	 */
+	private void triangle() {
+		for(int i = 0; i < mod.getAllFace().length; i++) {
+			gc.setFill(Color.GREY);
+			Face face = mod.getFaceAtIndex(i);
+			Point[] points = face.getTabp();
+			if(face.getCouleur()!= null) {
+				int[] colors = face.getCouleur();
+				gc.setFill(Color.rgb(colors[0], colors[1], colors[2]));
+			}
+			if(mod.getCheckF())gc.fillPolygon(new double[]{points[0].getX()+(canvas.getWidth()/2),points[1].getX()+(canvas.getWidth()/2),points[2].getX()+(canvas.getWidth()/2)},
+					new double[]{points[0].getY()+(canvas.getHeight()/2),points[1].getY()+(canvas.getHeight()/2),points[2].getY()+(canvas.getHeight()/2)},3);
+			if(mod.getCheckT())gc.strokePolygon(new double[]{points[0].getX()+(canvas.getWidth()/2),points[1].getX()+(canvas.getWidth()/2),points[2].getX()+(canvas.getWidth()/2)},
+					new double[]{points[0].getY()+(canvas.getHeight()/2),points[1].getY()+(canvas.getHeight()/2),points[2].getY()+(canvas.getHeight()/2)},3);
+		}
+	}
+
+	/**
+	 * Methode pour mettre à jour l'affichage en effaçant l'écran et en triant le modèle.
+	 */
+	public void update(Observable o, Object arg) {
+		try {
+		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		mod.triZ();
+		triangle();
+		} catch(Exception e) {
+			
+		}
+
+	}
+
+}
+

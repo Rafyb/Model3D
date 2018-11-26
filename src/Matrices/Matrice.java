430c5c367daa085eba5926fa2ba3208bd587d438
BauvinRaphael <raphael.bauvin@etudiant.univ-lille1.fr>
2018-11-26 11:25:25 +0100

    pas utile

--- src/Matrices/Matrice.java
+++ src/Matrices/Matrice.java
@@ -11,4 +11,40 @@ public class Matrice {
 		
 	}
 
+	public double getxA() {
+		return xA;
+	}
+
+	public double getxB() {
+		return xB;
+	}
+
+	public double getxC() {
+		return xC;
+	}
+
+	public double getyA() {
+		return yA;
+	}
+
+	public double getyB() {
+		return yB;
+	}
+
+	public double getyC() {
+		return yC;
+	}
+
+	public double getzA() {
+		return zA;
+	}
+
+	public double getzB() {
+		return zB;
+	}
+
+	public double getZc() {
+		return Zc;
+	}
+
 }
---
+++ src/Vue/OtherVue.java
@@ -0,0 +1,13 @@
+package Vue;
+
+public class OtherVue {
+	
+	public void vueEnCoupe() {
+		
+	}
+	
+	public void vueRotatif() {
+		
+	}
+
+}

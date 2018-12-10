29032a07935cb43126c59b88032d289d3c05da24
BauvinRaphael <raphael.bauvin@etudiant.univ-lille1.fr>
2018-12-10 10:38:31 +0100

    Ajout de ombre et vue en coupe à l'IHM

--- src/Vue/OtherVue.java
+++ src/Vue/OtherVue.java
@@ -1,9 +1,29 @@
 package Vue;
 
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class OtherVue {
+	private GraphicsContext gc;
+	private Canvas canvas;
 	
 	public void vueEnCoupe() {
+		canvas = new Canvas (500, 500);
+		gc = canvas.getGraphicsContext2D();
 		
+		VBox root = new VBox();
+		root.getChildren().add(canvas);
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root));
+		stage.setMinWidth(800);
+		stage.setMinHeight(700);
+		stage.setTitle("Model 3D Afficheur");
+		stage.setResizable(true);
+		stage.show();
 	}
 	
 	public void vueRotatif() {

a9774745bcf2f070abe3b2215c37a92bf34c0d0f
BauvinRaphael <raphael.bauvin@etudiant.univ-lille1.fr>
2018-12-17 10:28:10 +0100

    rotation

--- src/Vue/OtherVue.java
+++ src/Vue/OtherVue.java
@@ -19,7 +19,7 @@ public class OtherVue {
 		this.mod = mod;
 	}
 
-	public void vueEnCoupe(){
+	public void rotAuto(){
 		canvas = new Canvas (800, 800);
 		gc = canvas.getGraphicsContext2D();
 		
@@ -30,13 +30,12 @@ public class OtherVue {
 		stage.setScene(new Scene(root));
 		stage.setMinWidth(800);
 		stage.setMinHeight(800);
-		stage.setTitle("Vue en coupe");
+		stage.setTitle("Vue rotation");
 		stage.setResizable(true);
 		stage.show();
 		
 		try {
-			System.out.println("Affichage Coupe Start");
-			affCoupe();
+			System.out.println("Affichage Rotation Auto");
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -44,47 +43,7 @@ public class OtherVue {
 		stage.close();
 	}
 	
-	private double[] getDoubleTab(ArrayList<Double> list) {
-		double[] re = new double[list.size()];
-		int idx = 0;
-		for(Double d : list) {
-			re[idx] = d.doubleValue();
-			idx++;
-		}
-		return re;
-	}
-	
-	private void affCoupe() throws InterruptedException {
-		Point[] points = mod.getAllPoints();
-		Arrays.sort(points);
-		
-		Point tmp = points[0];
-		
-		ArrayList<Double> tabx = new ArrayList<>();
-		ArrayList<Double> tabz = new ArrayList<>();
-		
-		System.out.println("Tri effectué");
-		
-		for(Point p : points) {
-			if(p.getY()==tmp.getY()) {
-				tabx.add(p.getX());
-				tabz.add(p.getZ());
-				System.out.println("add "+p.getY());
-			} else {
-				gc.clearRect(0, 0, 800, 800);
-				gc.fillPolygon(getDoubleTab(tabx), getDoubleTab(tabz), tabx.size());
 
-				Thread.sleep(5000);
-				System.out.println("On est en "+p.getY());
-				
-				tmp = p;
-				tabx.clear();
-				tabz.clear();
-				tabx.add(p.getX());
-				tabz.add(p.getZ());
-			}
-		}	
-	}
 	
 	public void vueRotatif() {
 		
